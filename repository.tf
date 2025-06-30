7e47f275ff9f5a38202f3467a7db63a88cd54510
StanislavYermolenko <stanislav.yermolenko@gmail.com>
2025-06-30 17:04:18 +0200

    fix: Clean repository structure and resolve security issues

--- repository.tf
+++
@@ -1,8 +0,0 @@
-resource "argocd_repository" "gitops_config" {
-  repo            = "git@github.com:StanislavYermolenko/gitops-config.git"
-  type            = "git"
-  name            = "gitops-config"
-  ssh_private_key = file("~/.ssh/id_rsa")
-  
-  depends_on = [null_resource.wait_for_argocd]
-}
